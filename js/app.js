ebf9fbc8d87f907d2cc94cd0fa750373b1ea3fcd
Christopher <cchacon1998@yahoo.com>
2023-08-01 03:34:24 -0700

    updated work

--- js/app.js
+++ js/app.js
@@ -1,93 +1,33 @@
-    window.onload = (e) => {
-        const getCharInfo = () => {
-        fetch("https://swapi.dev/api/people").then((data) => {
-            return data.json()
-            }, (err) => {
-                console.log(err, ' ERROR')
-            }).then((char) => {
-                const userInput = document.querySelector('#input').value
-                // console.log(userInput)
-                // console.log(char.results)
-                const getNames = () => {
-                    const charArrays = char.results
-                    // console.log(charArrays)
-                    for(let i = 0; i < charArrays.length; i++) {
-                        let names = charArrays[i].name
-                        const charObjects = charArrays[i]
-                        // console.log(charObjects)
-                        // console.log(names)
-                        if(names.toLowerCase().includes(userInput.toLowerCase())) {
-                            document.querySelector('#name').innerText = charObjects.name
-                            document.querySelector('#height').innerText = charObjects.height
-                            document.querySelector('#mass').innerText = charObjects.mass
-                            document.querySelector('#hair').innerText = charObjects.hair_color
-                            document.querySelector('#skin').innerText = charObjects.skin_color
-                        }
-                    } 
-                }
-                getNames()
-            })
-        }
-         document.querySelector('#submit').addEventListener('click', (e) => {
-            e.preventDefault()
-            getCharInfo()
+ window.onload = (e) => {
+    const displayCharInfo = (characterInfo) => {
+        fetch(`https://swapi.dev/api/people/?search=${characterInfo}`).then((response) => response.json()).then((data) => {
+            const char = data.results[0]
+            if(char) {
+                document.querySelector('#modalName').innerText = char.name
+                document.querySelector('#modalHeight').innerText = char.height
+                document.querySelector('#modalMass').innerText = char.mass
+                document.querySelector('#modalHair').innerText = char.hair_color
+                document.querySelector('#modalSkin').innerText = char.skin_color
+                document.querySelector('#modalBirth').innerText = char.birth_year
+                document.querySelector('#modalEye').innerText = char.eye_color
+            }
+             const modal = document.getElementById('charModal')
+                 modal.style.display = 'block'
+        }).catch((error) => {
+            console.log('error')
         })
-    }
-
-
-   
-
-// fetch("https://swapi.dev/api/people/") 
-// .then(response => response.json()) 
-// .then(response => {
-//     console.log(response)
-//     console.log(response.results)
-// })
-// .catch(err => {
-// console.log(err)
-// })
-
-// fetch("https://swapi.dev/api/planets/")
-// .then(response => {
-//     console.log(response)
-// })
-// .catch(err => {
-//     console.log(err)
-// })
-
-
-// fetch("https://swapi.dev/api/films/")
-// .then(response => {
-//     console.log(response)
-// })
-// .catch(err => {
-//     console.log(err)
-// })
-
-
-// fetch("https://swapi.dev/api/species/")
-// .then(response => {
-//     console.log(response)
-// })
-// .catch(err => {
-//     console.log(err)
-// })
-
-
-// window.onload = fetch("https://swapi.dev/api/vehicles/")
-// .then(response => {
-//     console.log(response)
-// })
-// .catch(err => {
-//     console.log(err)
-// })
-
-
-
-// fetch("https://swapi.dev/api/starships/")
-// .then(response => {
-//     console.log(response)
-// })
-// .catch(err => {
-//     console.log(err)
-// })
+     }
+    const characterEl = document.querySelectorAll('.container ul li')
+    characterEl.forEach((characterEl) => {
+        characterEl.addEventListener('click', (e) => {
+            const characterInfo = e.currentTarget.textContent
+            displayCharInfo(characterInfo)
+        })
+    })
+    const closeModal = document.getElementById('closeModal')
+    closeModal.addEventListener('click', () => {
+        const modal = document.getElementById('charModal')
+        modal.style.display = 'none'
+    })
+}
+  
